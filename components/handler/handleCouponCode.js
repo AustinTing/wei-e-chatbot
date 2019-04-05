9dcd74da9b34daa50eef6d8a875c5da75474a47d
CellBody <austintingwork@gmail.com>
2019-04-05 16:12:11 +0800

    Add coupon code url

--- components/handler/handleCouponCode.js
+++ components/handler/handleCouponCode.js
@@ -1,6 +1,6 @@
-const { COUPON_NOTES } = process.env
-module.exports = async context => {
-  const reply = `"目前沒有折扣碼發送
+const { COUPON_NOTES, COUPON_NOTES_URL } = process.env
+const { Line } = require('messaging-api-line')
+const reply = `"目前沒有折扣碼發送
 --------------------
 折扣碼使用方式：
 1. 只適用於台灣收貨地址的訂單。
@@ -11,5 +11,11 @@ module.exports = async context => {
 6. 此活動主辦單位可以根據本活動的實際舉辦情況對活動規則進行變動或調整，本活動參加者視為同意本活動辦法及注意事項並受其拘束，活動辦法及注意事項嗣後如經主辦單位變更或修改者亦同。 
 7. 用戶不得要求更換其他物品或將獎品轉讓或折換成現金。獎品一經兌換、簽收受領後，如有 遺失、盜領、自行拋棄、毀損，均由得獎者自行承擔相關損害及損失，概與主辦單位無關。
 詳細說明：${COUPON_NOTES}`
-  return context.replyText(reply)
+module.exports = async context => {
+  return context.reply([
+    Line.createText(reply),
+    Line.createImage({
+      originalContentUrl: COUPON_NOTES_URL
+    })
+  ])
 }
